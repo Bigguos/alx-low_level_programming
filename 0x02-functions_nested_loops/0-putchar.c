6008cd577946bde7ff69fb7bfc9ebeb2c8dc5d49
Bigguos <guosaaide10@gmail.com>
2022-07-05 12:00:34 -0700

    az

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - A program that prints _putchar
+ *
+ *Return: Always (Success)
+ */
+
+int main(void)
+{
+int n;
+
+for (n = '3')
+if (n > 2)
+{
+printf("%d is _putchar\n", n);
+}
+return (0);
+}
