03afb316a82b4cd212a9942b135c840d4da54b99
Bigguos <guosaaide10@gmail.com>
2022-07-06 13:59:28 -0700

    eleventh task

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include <studio.h>
+
+/**
+ * print_to_98(int n)
+ * @n: The integer to be checked
+ *
+ * Return: no return
+ * */
+
+void print_to_98(int n)
+{
+if (n > 98)
+{
+for (; n > 98; n--)
+{
+printf("%d, ", n);
+}
+}
+printf("%d\n", n);
+}
