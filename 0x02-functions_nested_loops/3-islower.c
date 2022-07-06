3c6b4de9b0069a98a92441225affdd7d0f4658e7
Bigguos <guosaaide10@gmail.com>
2022-07-06 12:40:46 -0700

    fourth task

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,10 +5,10 @@
  */
 
 #include "main.h"
-int _islower(int c) 
-{  
-if (c >= 'a' && c <= 'z')    
-return (1);  
-else    
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+else
 return (0);
 }
