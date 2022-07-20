c272d9a98ae82f4d383558062ea8b66b852697fb
Bigguos <guosaaide10@gmail.com>
2022-07-20 15:38:27 -0700

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - print a string, followed by a new line
+ * @s: pointer to a string variable
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	for (; *s != '\0'; s++=)
+	{
+		_putchar(*s);
+	}
+	_putchar('\n');
