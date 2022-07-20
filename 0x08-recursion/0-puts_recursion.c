c9ecf309b52bed52250fd2e7cf526d18a5fe0f28
Bigguos <guosaaide10@gmail.com>
2022-07-20 15:58:14 -0700

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints a string, followed by a new line.
+ * _puts_recursion - prints a string, followed by a new line, to stdout
+ * _putchar: function
  * @s: pointer to a string variable
  * Return: void
  */
