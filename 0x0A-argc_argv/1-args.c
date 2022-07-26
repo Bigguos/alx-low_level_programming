dd52cf65c4ee4c7685747606a3b3a3f3c4e2a265
Bigguos <guosaaide10@gmail.com>
2022-07-26 08:30:00 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ * main - prints the number of arguments passed into it.
+ * @argc: number of command line arguments
+ * Return: 0 - Success
+ */
+
+int main(int argc, char* argv[]);
+{
+	int i;
+	printf("argc = %s\n", i, argv[i]);
+	return 0;
+}
