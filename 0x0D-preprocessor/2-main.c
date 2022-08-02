de2de57a52582262240e52f197fb9bcf6cb6bd58
Bigguos <guosaaide10@gmail.com>
2022-08-02 12:47:08 -0700

    task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define __FILE__
+
+/**
+ * main - prints the name of the file it was compiled from, followed by a new line
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("File :%s\n", __FILE__ );
+}
+return (0);
